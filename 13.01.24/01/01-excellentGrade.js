f5bf73021c21c06ebe4a417e2fbf5c58771b6873
mxrto <122569582+mxrtindnv@users.noreply.github.com>
2024-02-19 16:43:26 +0200

    Delete 13.01.24/01 directory

--- 13.01.24/01/01-excellentGrade.js
+++
@@ -1,7 +0,0 @@
-function gradeReturn(input){
-  let grade = Number(input[0]);
-  if(grade >= 5.5){
-    console.log("Excellent!");
-  }
-}
-gradeReturn(["5.5"]);
--- 13.01.24/01/04-passwordGuess.js
+++
@@ -1,9 +0,0 @@
-function guessPassword(input){
- let guess = input[0];
- if(guess == "s3cr3t!P@ssw0rd"){
-    console.log("Welcome");
- }else{
-    console.log("Wrong password!");
- }
-}
-guessPassword(["s3cr34t!P@ssw0rd"]);
